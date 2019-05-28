ed4621f8fac7b7477ef32be6a363920fedd0fe50
Sergio Gil <sgilperez@gmail.com>
2019-05-28 15:41:07 +0100

    Initial commit

---
+++ magit-spinner.el
@@ -0,0 +1,32 @@
+;;; magit-spinner.el --- Shows a spinner during Magit async operations
+;;; Version: 0.1
+;;; Commentary:
+;; See https://github.com/porras/magit-spinner
+;;; Homepage: https://github.com/porras/magit-spinner
+;; Package-Requires: ((emacs "25") (magit "2.90.0") (spinner "1.7.3"))
+;;; Code:
+
+(require 'magit)
+(require 'spinner)
+
+(defvar magit-spinner-mode--spinner (spinner-create 'progress-bar))
+
+(defconst magit-spinner-mode--lighter
+  '(:eval (spinner-start-print magit-spinner-mode--spinner)))
+
+(define-minor-mode magit-spinner-mode
+  "Shows a spinner during Magit async operations (pull, push, fetch, etc.)"
+  :lighter magit-spinner-mode--lighter)
+
+(defun magit-spinner-mode-enable (&rest args)
+  (magit-spinner-mode 1))
+
+(defun magit-spinner-mode-disable (&rest args)
+  (magit-spinner-mode -1))
+
+(advice-add 'magit-run-git-async :before 'magit-spinner-mode-enable)
+(advice-add 'magit-process-finish :after 'magit-spinner-mode-disable)
+
+(provide 'magit-spinner)
+
+;;; magit-spinner.el ends here
